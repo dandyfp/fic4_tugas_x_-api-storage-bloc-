e0232cda57d0cc51d86ee5d766b00833da678bd5
dandyfp <dandyfp@sakutambah.id>
2023-05-19 20:44:49 +0700

    fic api

--- lib/json_serialization/lesson/product_model.dart
+++ lib/json_serialization/lesson/product_model.dart
@@ -2,35 +2,34 @@
 //
 //     final productModel = productModelFromMap(jsonString);
 
-import 'package:meta/meta.dart';
 import 'dart:convert';
 
 class ProductModel {
-    final int id;
-    final String title;
-    final int price;
-    final String description;
-    final List<String> images;
-    final DateTime creationAt;
-    final DateTime updatedAt;
-    final Category category;
+  final int id;
+  final String title;
+  final int price;
+  final String description;
+  final List<String> images;
+  final DateTime creationAt;
+  final DateTime updatedAt;
+  final Category category;
 
-    ProductModel({
-        required this.id,
-        required this.title,
-        required this.price,
-        required this.description,
-        required this.images,
-        required this.creationAt,
-        required this.updatedAt,
-        required this.category,
-    });
+  ProductModel({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.images,
+    required this.creationAt,
+    required this.updatedAt,
+    required this.category,
+  });
 
-    factory ProductModel.fromJson(String str) => ProductModel.fromMap(json.decode(str));
+  factory ProductModel.fromJson(String str) => ProductModel.fromMap(json.decode(str));
 
-    String toJson() => json.encode(toMap());
+  String toJson() => json.encode(toMap());
 
-    factory ProductModel.fromMap(Map<String, dynamic> json) => ProductModel(
+  factory ProductModel.fromMap(Map<String, dynamic> json) => ProductModel(
         id: json["id"],
         title: json["title"],
         price: json["price"],
@@ -39,9 +38,9 @@ class ProductModel {
         creationAt: DateTime.parse(json["creationAt"]),
         updatedAt: DateTime.parse(json["updatedAt"]),
         category: Category.fromMap(json["category"]),
-    );
+      );
 
-    Map<String, dynamic> toMap() => {
+  Map<String, dynamic> toMap() => {
         "id": id,
         "title": title,
         "price": price,
@@ -50,41 +49,41 @@ class ProductModel {
         "creationAt": creationAt.toIso8601String(),
         "updatedAt": updatedAt.toIso8601String(),
         "category": category.toMap(),
-    };
+      };
 }
 
 class Category {
-    final int id;
-    final String name;
-    final String image;
-    final DateTime creationAt;
-    final DateTime updatedAt;
+  final int id;
+  final String name;
+  final String image;
+  final DateTime creationAt;
+  final DateTime updatedAt;
 
-    Category({
-        required this.id,
-        required this.name,
-        required this.image,
-        required this.creationAt,
-        required this.updatedAt,
-    });
+  Category({
+    required this.id,
+    required this.name,
+    required this.image,
+    required this.creationAt,
+    required this.updatedAt,
+  });
 
-    factory Category.fromJson(String str) => Category.fromMap(json.decode(str));
+  factory Category.fromJson(String str) => Category.fromMap(json.decode(str));
 
-    String toJson() => json.encode(toMap());
+  String toJson() => json.encode(toMap());
 
-    factory Category.fromMap(Map<String, dynamic> json) => Category(
+  factory Category.fromMap(Map<String, dynamic> json) => Category(
         id: json["id"],
         name: json["name"],
         image: json["image"],
         creationAt: DateTime.parse(json["creationAt"]),
         updatedAt: DateTime.parse(json["updatedAt"]),
-    );
+      );
 
-    Map<String, dynamic> toMap() => {
+  Map<String, dynamic> toMap() => {
         "id": id,
         "name": name,
         "image": image,
         "creationAt": creationAt.toIso8601String(),
         "updatedAt": updatedAt.toIso8601String(),
-    };
+      };
 }
